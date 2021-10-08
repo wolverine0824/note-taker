0a194eb1dcd795a0b6e83e50dc7f1ba49b0712b3
logon pierce <logonpierce@gmail.com>
2021-10-08 12:27:43 -0400

    update

--- server.js
+++ server.js
@@ -1,84 +1,93 @@
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-var notes = require("./db/db.json")
+// Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const notes = require("./db/db.json")
+const cuid = require("cuid")
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("./public"))
 
-currentID = notes.length;
+// currentID = notes.length;
 
-// API Routes
+// Routes
 
-app.get("/api/notes", function (req, res) {
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
-    return res.json(notes);
+
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
+
+app.get("/api/notes", (req, res) => res.json(note));
+
+app.get("/api/notes", (req, res) => {
+ const newNote = req.body;
+ nowNote.id = cuid()
+ note.push(newNote);
+ res.json(newNote);
 });
 
-app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
+app.delete("/api/notes", (req, res) => {
+const myID = req.params.id;
 
-    newNote["id"] = currentID +1;
-    currentID++;
-    console.log(newNote);
-
-    notes.push(newNote);
-
-    rewriteNotes();
-
-    return res.status(200).end();
-});
-
-app.delete("/api/notes/:id", function (req, res) {
-    res.send('Got a DELETE request at /api/notes/:id')
-
-    var id = req.params.id;
-
-    var idLess = notes.filter(function (less) {
-        return less.id < id;
-    });
-
-    var idGreater = notes.filter(function (greater) {
-        return greater.id > id;
-    });
-
-    notes = idLess.concat(idGreater);
-
-    rewriteNotes();
-})
-
-// Access files in "public" folder
-
-app.use(express.static("public"));
-
-// HTML Routes
-
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-// Listen
-
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
-
-// Functions
-
-function rewriteNotes() {
-    fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
-        if (err) {
-            console.log("error")
-            return console.log(err);
-        }
-
-        console.log("Success!");
-    });
+for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id === myID) {
+        note.splice([i], 1);
+    }
 }
+res.end
+});
+
+
+//     res.send('Got a DELETE request at /api/notes/:id')
+
+//     var id = req.params.id;
+
+//     var idLess = notes.filter(function (less) {
+//         return less.id < id;
+//     });
+
+//     var idGreater = notes.filter(function (greater) {
+//         return greater.id > id;
+//     });
+
+//     notes = idLess.concat(idGreater);
+
+//     rewriteNotes();
+// })
+
+// // Access files in "public" folder
+
+// app.use(express.static("public"));
+
+// // HTML Routes
+
+// app.get("/notes", function (req, res) {
+//     res.sendFile(path.join(__dirname, "public/notes.html"));
+// });
+
+// app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "public/index.html"));
+// });
+
+// // Listen
+
+// app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+// });
+
+// // Functions
+
+// function rewriteNotes() {
+//     fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
+//         if (err) {
+//             console.log("error")
+//             return console.log(err);
+//         }
+
+//         console.log("Success!");
+//     });
+// }
